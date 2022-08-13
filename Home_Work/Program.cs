cea8b43a1f2aa290ff5ac9c22afc231662abea80
Алексей Шевченко <alex18w@mail.ru>
2022-08-13 19:18:05 +0500

    Работа над ошибками

--- Home_Work/Program.cs
+++ Home_Work/Program.cs
@@ -30,7 +30,7 @@ void Zadacha_36()
     PrintArray(array);
     int Sum = 0;
     
-    for (int i = 0; i < size; i+=2)
+    for (int i = 1; i < size; i+=2)
     {
         Sum = Sum + array[i];  
     }
